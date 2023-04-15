b156e2fc95c6d8c257f35fd57c2ba762d705611f
Quvonchbek048 <quvonchbeku048@gmail.com>
2023-04-15 15:55:29 +0500

    Team Quvonchbek

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import "./About.css";
 
 const About = (props) => {
     return (
         <>
-         <h1>Hello my name is {props.name}</h1>   
+            <h1>Hello my name is {props.name}</h1>
         </>
     );
 }
